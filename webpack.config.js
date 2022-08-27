65c8b425692648ce7bff16d0edaa23819143479b
j-irion <juliusirion@gmx.de>
2022-08-27 18:22:49 +0200

    Setup hmtl-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     index: "./src/index.js",
     notes: "./src/notes.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
