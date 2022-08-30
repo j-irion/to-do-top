86934a58551f3d624453794304bbe62f8b3c6a70
j-irion <juliusirion@gmx.de>
2022-08-30 20:27:58 +0200

    Make note creator reset on submit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
-import { displayNoteCreator, closeNoteCreator } from "./notes.js";
+import { displayNoteCreator, closeNoteCreator, Note } from "./notes.js";
+import { Project } from "./projects.js";
 
 let btnAddNote = document.getElementById("btn-add-note");
-console.log(btnAddNote);
 btnAddNote.onclick = displayNoteCreator;
 
 let btnModalClose = document.getElementsByClassName("close")[0];
@@ -12,4 +12,13 @@ window.onclick = function (event) {
   if (event.target == document.getElementById("notesModal")) {
     closeNoteCreator();
   }
+};
+
+let noteCreationForm = document.getElementById("note-creation-form");
+noteCreationForm.onsubmit = (e) => {
+  e.preventDefault();
+  document.querySelectorAll("input").forEach((e) => (e.value = ""));
+  document.querySelectorAll("select").forEach((e) => (e.selectedIndex = 0));
+  document.querySelector("textarea").value = "";
+  closeNoteCreator();
 };
