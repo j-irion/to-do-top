a0d92149932d0a3fe53d36a4647f180d2ecb339e
j-irion <juliusirion@gmx.de>
2022-08-30 19:42:52 +0200

    Add note factory

--- src/notes.js
+++ src/notes.js
@@ -8,3 +8,51 @@ export function closeNoteCreator() {
   let modal = document.getElementById("notesModal");
   modal.style.display = "none";
 }
+
+const Note = function (
+  title,
+  priority,
+  dueDate,
+  description,
+  project,
+  completed
+) {
+  let noteTitle = title;
+  let notePrio = priority;
+  let noteDueDate = dueDate;
+  let noteDesc = description;
+  let noteCompleted = completed;
+
+  return {
+    get title() {
+      return noteTitle;
+    },
+    set title(title) {
+      noteTitle = title;
+    },
+    get priority() {
+      return notePrio;
+    },
+    set priority(priority) {
+      notePrio = priority;
+    },
+    get dueDate() {
+      return noteDueDate;
+    },
+    set dueDate(dueDate) {
+      noteDueDate = dueDate;
+    },
+    get description() {
+      return noteDesc;
+    },
+    set description(description) {
+      noteDesc = description;
+    },
+    get completed() {
+      return noteCompleted;
+    },
+    set completed(completed) {
+      noteCompleted = completed;
+    },
+  };
+};
