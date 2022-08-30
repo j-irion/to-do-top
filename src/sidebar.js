3f8e485446be68c8322613b962e5ee791cde3a7c
j-irion <juliusirion@gmx.de>
2022-08-30 23:08:09 +0200

    Render projects

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,5 +1,14 @@
+import { app } from "./app";
+
 const sidebar = (() => {
-  const render = () => {};
+  const render = () => {
+    const projectsDiv = document.querySelector(".projects");
+    app.projects.forEach((proj) => {
+      let project = document.createElement("span");
+      project.textContent = proj.name;
+      projectsDiv.appendChild(project);
+    });
+  };
 
   return {
     render,
