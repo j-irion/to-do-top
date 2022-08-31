462d687aa1c9e70f5d73e8ab9729979a47dc7052
j-irion <juliusirion@gmx.de>
2022-08-31 23:11:38 +0200

    Render inbox upon refresh

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./style.css";
 import { sidebar } from "./sidebar.js";
+import { content } from "./content.js";
+import { app } from "./app.js";
 
 sidebar.render();
+
+content.renderProject(app.projects[0]);
