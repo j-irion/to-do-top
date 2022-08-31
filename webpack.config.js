f5cafbf8f3b6d1360a99e3a741a14dc8ee9f3879
j-irion <juliusirion@gmx.de>
2022-08-31 17:44:25 +0200

    Create and link content.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     sidebar: "./src/sidebar.js",
     modal: "./src/modal.js",
     header: "./src/header.js",
+    conent: "./src/content.js",
   },
   devtool: "inline-source-map",
   plugins: [
