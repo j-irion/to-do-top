5d3cd6604420eb742250f297e35104ccea566050
j-irion <juliusirion@gmx.de>
2022-09-01 16:28:20 +0200

    Create render note function

--- src/content.js
+++ src/content.js
@@ -14,6 +14,9 @@ const content = (() => {
       let noteDiv = document.createElement("div");
       noteDiv.textContent = note.title;
       contentDiv.appendChild(noteDiv);
+      noteDiv.addEventListener("click", () => {
+        renderNote(note, noteDiv);
+      });
       let edit = document.createElement("span");
       edit.className = "material-symbols-rounded";
       edit.innerHTML = "edit";
@@ -23,6 +26,50 @@ const content = (() => {
         displayNoteEditor(note);
       });
     });
+  };
+
+  const renderNote = (note, noteDiv) => {
+    let noteDivOpen = document.createElement("div");
+    noteDivOpen.className = "note-div-open";
+    let noteDivOpenColumnOne = document.createElement("div");
+    noteDivOpenColumnOne.className = "note-div-open-column";
+    let noteDivOpenColumnTwo = document.createElement("div");
+    noteDivOpenColumnTwo.className = "note-div-open-column";
+    noteDivOpen.appendChild(noteDivOpenColumnOne);
+    noteDivOpen.appendChild(noteDivOpenColumnTwo);
+    let titleElement = document.createElement("div");
+    let dateElement = document.createElement("div");
+    noteDivOpenColumnOne.appendChild(titleElement);
+    noteDivOpenColumnOne.appendChild(dateElement);
+    let descElement = document.createElement("div");
+    let prioElement = document.createElement("div");
+    noteDivOpenColumnTwo.appendChild(descElement);
+    noteDivOpenColumnTwo.appendChild(prioElement);
+    let titleHeading = document.createElement("h4");
+    titleHeading.textContent = "Title:";
+    let title = document.createElement("span");
+    title.textContent = note.title;
+    titleElement.appendChild(titleHeading);
+    titleElement.appendChild(title);
+    let dateHeading = document.createElement("h4");
+    dateHeading.textContent = "Date:";
+    let date = document.createElement("span");
+    date.textContent = note.dueDate;
+    dateElement.appendChild(dateHeading);
+    dateElement.appendChild(date);
+    let descHeading = document.createElement("h4");
+    descHeading.textContent = "Description:";
+    let desc = document.createElement("span");
+    desc.textContent = note.description;
+    descElement.appendChild(descHeading);
+    descElement.appendChild(desc);
+    let prioHeading = document.createElement("h4");
+    prioHeading.textContent = "Priority:";
+    let prio = document.createElement("span");
+    prio.textContent = note.priority;
+    prioElement.appendChild(prioHeading);
+    prioElement.appendChild(prio);
+    noteDiv.appendChild(noteDivOpen);
   };
 
   return {
